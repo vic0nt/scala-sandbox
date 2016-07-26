184f8626337744def91b0f7ff730aad24bdba387
Victor Tsvetkov <stole.the.groove@gmail.com>
2016-07-26 11:32:51 +0300

    initial commit

---
+++ hackerrank.com/src/thirty/days/of/code/Day6.scala
@@ -0,0 +1,16 @@
+package thirty.days.of.code
+
+/**
+  * Created by ruatvv2 on 26.07.16.
+  */
+object Solution {
+
+  def main(args: Array[String]) {
+    val lines = io.Source.stdin.getLines
+    for (s <- lines.drop(1)) {
+      val odds = s.zipWithIndex.filter(_._2 % 2 == 0).map(_._1).mkString("")
+      val evens = s.zipWithIndex.filter(_._2 % 2 == 1).map(_._1).mkString("")
+      println(s"$odds $evens")
+    }
+  }
+}
