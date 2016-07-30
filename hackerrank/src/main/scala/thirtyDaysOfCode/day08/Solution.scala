62c146b234a17c9bb731878dcf6bd24abb8ff3b8
vicont <stole.the.groove@gmail.com>
2016-07-30 09:51:04 +0300

    project reorganization

---
+++ hackerrank/src/main/scala/thirtyDaysOfCode/day08/Solution.scala
@@ -0,0 +1,12 @@
+package thirtyDaysOfCode.day08
+
+/**
+  * Created by vicont on 30.07.2016.
+  */
+object Solution {
+  def main(args: Array[String]) {
+    val lines = io.Source.stdin.getLines
+    val qty = lines.take(1)
+    println(qty)
+  }
+}
