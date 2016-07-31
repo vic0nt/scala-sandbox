2079f46bf3ca5e66538823e3dcfc977fffffaa49
vicont <stole.the.groove@gmail.com>
2016-07-31 16:56:09 +0300

    day 08

--- hackerrank/src/main/scala/thirtyDaysOfCode/day08/Solution.scala
+++ hackerrank/src/main/scala/thirtyDaysOfCode/day08/Solution.scala
@@ -1,12 +1,30 @@
 package thirtyDaysOfCode.day08
 
+import java.util.Scanner
+
 /**
   * Created by vicont on 30.07.2016.
   */
 object Solution {
   def main(args: Array[String]) {
-    val lines = io.Source.stdin.getLines
-    val qty = lines.take(1)
-    println(qty)
+
+    def findNumber(phoneBook: Map[String, Int], name: String) = phoneBook.get(name) match {
+      case Some(number) => s"$name=$number"
+      case None => "Not found"
+    }
+
+    val in = new Scanner(System.in)
+    val n = in.nextInt()
+    val phoneBook = (for (_ <- 1 to n) yield {
+      val name = in.next()
+      val phone = in.nextInt()
+      name -> phone
+    }).toMap
+
+    while(in.hasNext()){
+      val s = in.next()
+      println(findNumber(phoneBook,s))
+    }
+    in.close()
   }
 }
