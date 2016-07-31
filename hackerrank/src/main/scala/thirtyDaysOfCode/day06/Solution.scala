aa5ea03afc31dae5c05367042dbc6c175ad4d7cd
vicont <stole.the.groove@gmail.com>
2016-07-31 20:50:32 +0300

    day 06 refactoring

--- hackerrank/src/main/scala/thirtyDaysOfCode/day06/Solution.scala
+++ hackerrank/src/main/scala/thirtyDaysOfCode/day06/Solution.scala
@@ -8,9 +8,11 @@ object Solution {
   def main(args: Array[String]) {
     val lines = io.Source.stdin.getLines
     for (s <- lines.drop(1)) {
-      val odds = s.zipWithIndex.filter(_._2 % 2 == 0).map(_._1).mkString("")
-      val evens = s.zipWithIndex.filter(_._2 % 2 == 1).map(_._1).mkString("")
-      println(s"$odds $evens")
+      val rez = s.zipWithIndex.partition(_._2 % 2 == 0) match {
+        case (odds, evens) => (odds.map(el => el._1).mkString(""), evens.map(el => el._1).mkString(""))
+        case _ => (Nil, Nil)
+      }
+      println(rez._1 + " " + rez._2)
     }
   }
 }
