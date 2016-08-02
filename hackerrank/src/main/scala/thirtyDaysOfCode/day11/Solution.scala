9df056de5225f41521a2659b208d1f9abc83ea10
vicont <stole.the.groove@gmail.com>
2016-08-02 23:34:36 +0300

    day 11

--- hackerrank/src/main/scala/thirtyDaysOfCode/day11/Solution.scala
+++ hackerrank/src/main/scala/thirtyDaysOfCode/day11/Solution.scala
@@ -2,21 +2,27 @@ package thirtyDaysOfCode.day11
 
 object Solution {
   def main(args: Array[String]) {
+    val sc = new java.util.Scanner (System.in)
+    val arr = Array.ofDim[Int](6,6)
+    for(arr_i <- 0 until 6) {
+      for(arr_j <- 0 until 6){
+        arr(arr_i)(arr_j) = sc.nextInt()
+      }
+    }
 
-    def hourglassSum(a: Array[Array[Int]]) = a.flatten.zipWithIndex.filter(e => e._2 != 3 || e._2 != 5).map(e => e._1).sum
+    def getHourglassSum(a: Array[Array[Int]], row: Int, col: Int) = {
+      val sublist = for {
+        i <- row to row + 2
+        j <- col to col + 2
+      } yield a(i)(j)
+      sublist.zipWithIndex.filter(e => e._2 != 3 && e._2 != 5).map(e => e._1).sum
+    }
 
+    val rez = for {
+      i <- 0 to 3
+      j <- 0 to 3
+    } yield getHourglassSum(arr, i, j)
 
-    val arr = Array.ofDim[Int](3,3)
-    arr(0)(0) = 2
-    arr(0)(1) = 4
-    arr(0)(2) = 4
-    arr(1)(0) = 0
-    arr(1)(1) = 2
-    arr(1)(2) = 0
-    arr(2)(0) = 1
-    arr(2)(1) = 2
-    arr(2)(2) = 4
-
-    //print(hourglassSum(arr))
+    print(rez.max)
   }
 }
