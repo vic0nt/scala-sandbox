bd822e94d97e40c0700d4d24b491978f9d3ae793
vicont <stole.the.groove@gmail.com>
2016-09-04 18:21:11 +0300

    day 29

---
+++ hackerrank/src/main/scala/thirtyDaysOfCode/day29/Solution.scala
@@ -0,0 +1,31 @@
+package thirtyDaysOfCode.day29
+
+/**
+  * Created by vicont on 04.09.2016.
+  * THIS SOLUTION IS POOR, IT HAS O(N * N) COMPLEXITY
+  */
+object Solution {
+
+  def getBitAndMax(N: Int, K: Int): Int = {
+    (for {
+      i <- 1 to N
+      j <- 1 to N
+    } yield (i, j))
+      .filter(p => p._1 != p._2 && p._1 < p._2)
+      .map(p => p._1 & p._2)
+      .sorted
+      .takeWhile(_ < K).last
+  }
+
+  def main(args: Array[String]) {
+    val in = new java.util.Scanner(System.in)
+    val n = in.nextInt()
+    val pairs = for (_ <- 1 to n) yield {
+      val N = in.nextInt()
+      val K = in.nextInt()
+      (N, K)
+    }
+    pairs.map(e => getBitAndMax(e._1,e._2)).foreach(println)
+  }
+}
+
