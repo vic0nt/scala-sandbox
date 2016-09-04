dde577ab0d28c137184cba4b1dd118973a8d2024
vicont <stole.the.groove@gmail.com>
2016-09-04 16:56:50 +0300

    day 27

---
+++ hackerrank/src/main/scala/thirtyDaysOfCode/day27/Solution.scala
@@ -0,0 +1,15 @@
+package thirtyDaysOfCode.day27
+
+/**
+  * Created by vicont on 04.09.2016.
+  */
+object Solution {
+  def main(args: Array[String]) = {
+    println("5")
+    println("5 3"); println("-1 90 999 100 0")
+    println("4 2"); println("0 -1 2 1")
+    println("3 3"); println("-1 0 1")
+    println("6 1"); println("-1 0 1 -1 2 3")
+    println("7 3"); println("-1 0 1 2 3 4 5")
+  }
+}
