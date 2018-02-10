cc000806df6198fb5a703b5335864e7dacd531d7
vicont <stole.the.groove@gmail.com>
2018-02-10 19:17:05 +0300

    The Red Book excercises: chapter 3

--- scala-fp/src/ch3/List.scala
+++ scala-fp/src/ch3/List.scala
@@ -3,9 +3,7 @@ package ch3
 import scala.annotation.tailrec
 
 sealed trait List[+A]
-
 case object Nil extends List[Nothing]
-
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 object List {
