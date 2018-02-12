a9bc2e23cdb54379e96ef67c7e5d14862ad3065f
Victor Tsvetkov <viktor.tsvetkov@raiffeisen.ru>
2018-02-12 11:30:11 +0300

    The Red Book excercises: chapter 3

---
+++ scala-fp/src/ch3/Ex26.scala
@@ -0,0 +1,18 @@
+package ch3
+
+object Ex26 extends App {
+
+  import Tree._
+
+  val l01 = Leaf(10)
+  val l02 = Leaf(20)
+  val b01 = Branch(l01, l02)
+
+  val l11 = Leaf(100)
+  val l12 = Leaf(200)
+  val b11 = Branch(l11, l12)
+
+  val tree = Branch(b01, b11)
+
+  assert(maximum(tree) == 200)
+}
