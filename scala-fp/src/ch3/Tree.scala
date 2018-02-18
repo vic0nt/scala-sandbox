36e6f75b0f7cbe477e04defbd5d2fc43d03472d2
vicont <stole.the.groove@gmail.com>
2018-02-18 14:25:14 +0300

    The Red Book excercises: chapter 3

--- scala-fp/src/ch3/Tree.scala
+++ scala-fp/src/ch3/Tree.scala
@@ -18,4 +18,26 @@ object Tree {
     case Branch(l, r) ⇒ maximum(l) max maximum(r)
   }
 
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) ⇒ 0
+    case Branch(l,r) ⇒ 1 + (depth(l) max depth(r))
+  }
+
+  def map[A,B](t: Tree[A])(f: A ⇒ B): Tree[B] = t match {
+    case Leaf(n) ⇒ Leaf(f(n))
+    case Branch(l,r) ⇒ Branch(map(l)(f), map(r)(f))
+  }
+
+  def fold[A,B](t: Tree[A])(f: A ⇒ B)(g: (B,B) ⇒ B): B = t match {
+    case Leaf(n) ⇒ f(n)
+    case Branch(l,r) ⇒ g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def sizeViaFold[A](t: Tree[A]): Int = fold(t)(_ ⇒ 1)((l,r) ⇒ 1 + l + r)
+
+  def maximumViaFold(t: Tree[Int]): Int = fold(t)(a ⇒ a)((l,r) ⇒ l max r)
+
+  def depthViaFold[A](t: Tree[A]): Int = fold(t)(_ ⇒ 0)((l,r) ⇒ 1 + (l max r))
+
+  //def mapViaFold[A,B](t: Tree[A])(f: A ⇒ B): Tree[B] = fold(t)(_ ⇒ Leaf(f(_)): Tree[B])((l,r) ⇒ Branch(l,r))
 }
